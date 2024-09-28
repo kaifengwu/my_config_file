a91d035b46866f973198b4b169480b4996f48846
KaifengWU <core.editor>
2024-09-28 23:47:06 +0800

    add function to open and close vpn

--- autoinstall.sh
+++ autoinstall.sh
@@ -6,4 +6,11 @@ set files (ls -a ./ | grep -E '(^\.[^.]+$|\.tmux.conf)' |grep -v '\.git$')
 for file in $files
     ln -s (pwd)/$file ~/$file # 创建软链接
 end
-     
+
+ll -a ~/ | grep '^.*[0-9] \.config$'
+if test $status -eq 0
+    ln -s (pwd)/fish ~/.config/fish
+else 
+    mkdir ~/.config
+    ln -s (pwd)/fish ~/.config/fish
+end
