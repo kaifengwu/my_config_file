da592523c16f94ecd79087d10f54b467a949f26c
KaifengWU <core.editor>
2025-02-28 11:42:45 +0800

    fish快捷键alt

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,23 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+
+# >>> coursier install directory >>>
+set -gx PATH "$PATH:/home/kaifeng/.local/share/coursier/bin"
+set -gx JAVA_HOME /usr/lib/jvm/java-17-openjdk-amd64
+set -gx PATH $JAVA_HOME/bin $PATH
+# <<< coursier install directory <<<
+
+function smart_ctrl_l
+    if test -n (commandline) 
+        commandline -f forward-char # 有输入时向右移动光标
+    else
+        clear; commandline -f repaint # 无输入时清屏
+    end
+end
+
+bind \cl smart_ctrl_l  # 绑定 Ctrl-l
 set -Ux NVBOARD_HOME /home/kaifeng/ysyx-workbench/nvboard
 set -Ux NEMU_HOME /home/kaifeng/ysyx-workbench/nemu
 
@@ -16,9 +33,8 @@ if not type -q capslock
 end
 
 set -gx PATH "$PATH:/home/kaifeng/.cache/scalacli/local-repo/bin/scala-cli"
-
+ 
 # >>> coursier install directory >>>
 set -gx PATH "$PATH:/home/kaifeng/.local/share/coursier/bin"
 set -gx JAVA_HOME /usr/lib/jvm/java-17-openjdk-amd64
-set -gx PATH $JAVA_HOME/bin $PATH
-# <<< coursier install directory <<<
+set -gx PATH $PATH /usr/lib/jvm/java-17-openjdk-amd64/bin /home/kaifeng/.fzf/bin /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games /usr/local/games /snap/bin /snap/bin /home/kaifeng/.cache/scalacli/local-repo/bin/scala-cli /home/kaifeng/.local/share/coursier/bin /home/kaifeng/.local/bin /home/kaifeng/.local/lib/python3.10/site-packages /opt/riscv-linux/bin /home/kaifeng/qemu/build
